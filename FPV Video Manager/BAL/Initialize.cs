96a5bb5f52c589104432e3c082bedd939a05d3a3
ryan susman <ryansusman@gmail.com>
2019-07-05 20:06:02 -0500

    First Commit, Base Framework

---
+++ FPV Video Manager/BAL/Initialize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPV_Video_Manager.BAL
+{
+    class Initialize
+    {
+        public Initialize()
+        {
+            DAL.FPVVideoMigratorConfiguration fPV_VMC = new DAL.FPVVideoMigratorConfiguration();
+        }
+    }
+}
---
+++ FPV Video Manager/DAL/FPVVideoMigratorConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FPV_Video_Configuration.Configuration;
+
+namespace FPV_Video_Manager.DAL
+{
+    class FPVVideoMigratorConfiguration
+    {
+        Manager configurationManager = new Manager();
+    }
+}
