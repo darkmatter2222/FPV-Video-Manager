e3cb627d48b992c0530a171b6d5208b96161422c
ryan susman <ryansusman@gmail.com>
2019-08-09 10:32:35 -0500

    Checkpoint

---
+++ FPV Video Manager/MainWindowV2.xaml.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace FPV_Video_Manager
+{
+    /// <summary>
+    /// Interaction logic for MainWindowV2.xaml
+    /// </summary>
+    public partial class MainWindowV2 : Window
+    {
+        public MainWindowV2()
+        {
+            DataContext = new ListsAndGridsViewModel();
+            InitializeComponent();
+        }
+    }
+
+    public class ListsAndGridsViewModel : INotifyPropertyChanged
+    {
+        private readonly ObservableCollection<SelectableViewModel> _items3;
+        private bool? _isAllItems3Selected;
+
+        public ListsAndGridsViewModel()
+        {
+            _items3 = CreateData();
+        }
+
+        public bool? IsAllItems3Selected
+        {
+            get { return _isAllItems3Selected; }
+            set
+            {
+                if (_isAllItems3Selected == value) return;
+
+                _isAllItems3Selected = value;
+
+                if (_isAllItems3Selected.HasValue)
+                    SelectAll(_isAllItems3Selected.Value, Items3);
+
+                OnPropertyChanged();
+            }
+        }
+
+        private static void SelectAll(bool select, IEnumerable<SelectableViewModel> models)
+        {
+            foreach (var model in models)
+            {
+                model.IsSelected = select;
+            }
+        }
+
+        private static ObservableCollection<SelectableViewModel> CreateData()
+        {
+            return new ObservableCollection<SelectableViewModel>
+            {
+                new SelectableViewModel
+                {
+                    Code = 'M',
+                    Name = "Material Design",
+                    Description = "Material Design in XAML Toolkit",
+                    Compress = "False"
+                },
+                new SelectableViewModel
+                {
+                    Code = 'D',
+                    Name = "Dragablz",
+                    Description = "Dragablz Tab Control",
+                    Compress = "True",
+                    IsAudiable = true
+                },
+                new SelectableViewModel
+                {
+                    Code = 'P',
+                    Name = "Predator",
+                    Description = "If it bleeds, we can kill it",
+                    Compress = "False"
+                }
+            };
+        }
+
+        public ObservableCollection<SelectableViewModel> Items3 => _items3;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public IEnumerable<string> CompressItems
+        {
+            get
+            {
+                yield return "True";
+                yield return "False";
+            }
+        }
+
+    }
+    public class SelectableViewModel : INotifyPropertyChanged
+    {
+        private bool _isSelected;
+        private string _name;
+        private string _description;
+        private char _code;
+        private double _numeric;
+        private string _compress;
+        private bool _isAudiable;
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (_isSelected == value) return;
+                _isSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public char Code
+        {
+            get { return _code; }
+            set
+            {
+                if (_code == value) return;
+                _code = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == value) return;
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Description
+        {
+            get { return _description; }
+            set
+            {
+                if (_description == value) return;
+                _description = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Numeric
+        {
+            get { return _numeric; }
+            set
+            {
+                if (_numeric == value) return;
+                _numeric = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Compress
+        {
+            get { return _compress; }
+            set
+            {
+                if (_compress == value) return;
+                _compress = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public bool IsAudiable
+        {
+            get { return _isAudiable; }
+            set
+            {
+                if (_isAudiable == value) return;
+                _isAudiable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
